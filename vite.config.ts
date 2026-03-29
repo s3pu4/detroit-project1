e440da92b82a5a2aca1386751a161551671866e2
s3pu4 <jjken233j@gmail.com>
2026-03-29 20:35:59 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,3 +17,10 @@ export default defineConfig({
     },
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/detroit-project1/',
+})
